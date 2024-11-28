59652950023963e77c2685cae9fef60c732fcebb
Jordan Victor <jordanvictor63@gmail.com>
2024-11-28 12:40:06 -0400

    Função para adicionar uma nova tarefa em um quadro finalizada

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 const app = {  
     data(){
-        let lists = [
-           
-        ]
+        let lists = []
 
         return{
             newNote:{
+                data:'',
                 text:'',
                 tasks:[]
             },
             newTask:{done:false, task:''},
-            lists
+            lists,
+            n_quadros:''
         }
     },
 
+
     methods:{
+        //ADICIONANDO NOVO QUADRO
         addList(){
             if(this.newNote.text != ''){
+                let dia = new Date().getDate()
+                let mes = new Date().getMonth()
+                let ano = new Date().getFullYear()
+                let data = `${dia}/${mes}/${ano}`
+                
+                this.newNote.data = data
+
                 this.lists.push(this.newNote)              
 
                 this.newNote = {
-
+                    data:'',
+                    text:'',
+                    tasks:[]
                 }
 
-                //Salvando no localStorage
                 localStorage.setItem("tarefa", JSON.stringify(this.lists))
             }
             else{
-                alert("Escreva algo na lista")
+                alert("Escreva o nome do quadro")
             }
         },
 
@@ -36,8 +46,13 @@ const app = {
         },
 
 
+
+
+
+        //ADICIONANDO NOVA TAREFA   
         addTask(i){
             if(this.newTask.task != ''){
+                console.log(i)
                 this.lists[i].tasks.push(this.newTask)
 
                 this.newTask = {
@@ -47,14 +62,51 @@ const app = {
                 localStorage.setItem("tarefa", JSON.stringify(this.lists))
             }
             else{
-                console.log("Adicione uma tarefa")
+                alert("Adicione uma tarefa")
             }
+        },
+
+
+
+
+
+        //DELETANDO QUADRO
+        saveDelete(){
+            localStorage.setItem("tarefa", JSON.stringify(this.lists))
+        },
+        
+        handleClick(){
+            
+        },
+
+
+
+
+        //CONTANDO O TOTAL DE QUADROS
+        total_quadros(){
+            let tt_quadros = document.getElementsByName("quadro")
+            this.n_quadros = tt_quadros.length
+            return this.n_quadros
         }
     },
     
+
+
+
+
+    //CARREGANDO QUADROS NA TELA
     created(){
-        this.lists = localStorage.getItem("tarefa") ? JSON.parse(localStorage.getItem("tarefa")) : this.lists
+        this.lists = localStorage.getItem("tarefa") ? JSON.parse(localStorage.getItem("tarefa")) : this.lists     
     },
+    //ATUALIZANDO NÚMERO DE QUADROS AÓS ADIÇÃO
+    updated(){
+        let tt_quadros = document.getElementsByName("quadro")
+        this.n_quadros = tt_quadros.length
+    },
+    //EXIBINDO O NUEMRO TOTAL DE QUADROS APÓS CARREGAMNETO DA PÁGINA
+    mounted(){
+        this.total_quadros()
+    }
     /*
     updated(){
         localStorage.setItem("tarefa", JSON.stringify(this.lists))
